d1970d0b4902c033e9669cc517342a3cc4c25e01
Maxim <74974283+maximka76667@users.noreply.github.com>
2024-12-06 12:21:01 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ import streamlit as st
 from model import model, vectorizer
 
 st.title("Spam Classifier")
-user_input = st.text_input("Enter a message:")
+user_input = st.text_input("Enter a message (English only):")
 
 if user_input:
     prediction = model.predict(vectorizer.transform([user_input]))
